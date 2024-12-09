e87f2f5101397402099ae5d92663f8645e15a954
yashma-sonara <yashma.sonara04@gmail.com>
2024-12-09 09:23:36 +0530

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,25 @@
+
+import Hero from '../../components/Hero';
+import { FloatingNav } from '../../components/ui/FloatingNav';
+import Grid from '../../components/Grid';
+import './globals.css';
+import {FaHome} from 'react-icons/fa';
+import RecentProjects from '../../components/RecentProjects';
+import { navItems } from '../../data';
+import Experience from '../../components/Experience';
+import Approach from '../../components/Approach';
+
+export default function Home() {
+  return (
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className='max-w-7xl w-full'>
+        <FloatingNav navItems={navItems}/>
+        <Hero/>
+        <Grid />
+        <RecentProjects />
+        <Experience />
+        
+      </div>
+    </main>
+  );
+}
